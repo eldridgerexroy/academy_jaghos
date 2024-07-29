0415e2f7ed9cfa7b6535c89bf7bb2530cdfc3228
eldridgerexroy <40339762+eldridgerexroy@users.noreply.github.com>
2024-07-29 23:25:06 +0800

    update home background hero1

--- resources/views/web/default/pages/home.blade.php
+++ resources/views/web/default/pages/home.blade.php
@@ -7,69 +7,68 @@
 
 @section('content')
 
-    @if(!empty($heroSectionData))
+@if(!empty($heroSectionData))
 
-        @if(!empty($heroSectionData['has_lottie']) and $heroSectionData['has_lottie'] == "1")
-            @push('scripts_bottom')
-                <script src="/assets/default/vendors/lottie/lottie-player.js"></script>
-            @endpush
-        @endif
+    @if(!empty($heroSectionData['has_lottie']) and $heroSectionData['has_lottie'] == "1")
+        @push('scripts_bottom')
+            <script src="/assets/default/vendors/lottie/lottie-player.js"></script>
+        @endpush
+    @endif
 
-        <section style="padding-bottom:0px" class="slider-container  {{ ($heroSection == "2") ? 'slider-hero-section2' : '' }}" @if(empty($heroSectionData['is_video_background'])) style="background-image: url('{{ $heroSectionData['hero_background'] }}')" @endif>
+    <section class="slider-container  {{ ($heroSection == "2") ? 'slider-hero-section2' : '' }}" @if(empty($heroSectionData['is_video_background'])) style="background-image: url('{{ $heroSectionData['hero_background'] }}')" @endif>
 
-            @if($heroSection == "1")
-                @if(!empty($heroSectionData['is_video_background']))
-                    <video playsinline autoplay muted loop id="homeHeroVideoBackground" class="img-cover">
-                        <source src="{{ $heroSectionData['hero_background'] }}" type="video/mp4">
-                    </video>
-                @endif
-
-                <div class="mask"></div>
+        @if($heroSection == "1")
+            @if(!empty($heroSectionData['is_video_background']))
+                <video playsinline autoplay muted loop id="homeHeroVideoBackground" class="img-cover">
+                    <source src="{{ $heroSectionData['hero_background'] }}" type="video/mp4">
+                </video>
             @endif
 
-            <div class="container user-select-none">
+            <div class="mask"></div>
+        @endif
 
-                @if($heroSection == "2")
-                    <div class="row slider-content align-items-center hero-section2 flex-column-reverse flex-md-row">
-                        <div class="col-12 col-md-7 col-lg-6">
-                            <h1 class="text-secondary font-weight-bold">{{ $heroSectionData['title'] }}</h1>
-                            <p class="slide-hint text-gray mt-20 text-justify">{!! nl2br($heroSectionData['description']) !!}</p>
+        <div class="container user-select-none">
 
-                            <form action="/search" method="3
-                            t" class="d-inline-flex mt-30 mt-lg-30 w-100">
+            @if($heroSection == "2")
+                <div class="row slider-content align-items-center hero-section2 flex-column-reverse flex-md-row">
+                    <div class="col-12 col-md-7 col-lg-6">
+                        <h1 class="text-secondary font-weight-bold text-justify">{{ $heroSectionData['title'] }}</h1>
+                        <p class="slide-hint text-gray mt-20">{!! nl2br($heroSectionData['description']) !!}</p>
+
+                        <form action="/search" method="get" class="d-inline-flex mt-30 mt-lg-30 w-100">
+                            <div class="form-group d-flex align-items-center m-0 slider-search p-10 bg-white w-100">
+                                <input type="text" name="search" class="form-control border-0 mr-lg-50" placeholder="{{ trans('home.slider_search_placeholder') }}"/>
+                                <button type="submit" class="btn btn-primary rounded-pill">{{ trans('home.find') }}</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-12 col-md-5 col-lg-6">
+                        @if(!empty($heroSectionData['has_lottie']) and $heroSectionData['has_lottie'] == "1")
+                            <lottie-player src="{{ $heroSectionData['hero_vector'] }}" background="transparent" speed="1" class="w-100" loop autoplay></lottie-player>
+                        @else
+                            <img src="{{ $heroSectionData['hero_vector'] }}" alt="{{ $heroSectionData['title'] }}" class="img-cover">
+                        @endif
+                    </div>
+                </div>
+            @else
+                <div class="text-center slider-content">
+                    <h1>{{ $heroSectionData['title'] }}</h1>
+                    <div class="row h-100 align-items-center justify-content-center text-center">
+                        <div class="col-12 col-md-9 col-lg-7">
+                            <p class="mt-30 slide-hint">{!! nl2br($heroSectionData['description']) !!}</p>
+
+                            <form action="/search" method="get" class="d-inline-flex mt-30 mt-lg-50 w-100">
                                 <div class="form-group d-flex align-items-center m-0 slider-search p-10 bg-white w-100">
                                     <input type="text" name="search" class="form-control border-0 mr-lg-50" placeholder="{{ trans('home.slider_search_placeholder') }}"/>
                                     <button type="submit" class="btn btn-primary rounded-pill">{{ trans('home.find') }}</button>
                                 </div>
                             </form>
                         </div>
-                        <div class="col-12 col-md-5 col-lg-6">
-                            @if(!empty($heroSectionData['has_lottie']) and $heroSectionData['has_lottie'] == "1")
-                                <lottie-player src="{{ $heroSectionData['hero_vector'] }}" background="transparent" speed="1" class="w-100" loop autoplay></lottie-player>
-                            @else
-                                <img src="{{ $heroSectionData['hero_vector'] }}" alt="{{ $heroSectionData['title'] }}" class="img-cover">
-                            @endif
-                        </div>
                     </div>
-                @else
-                    <div class="text-center slider-content">
-                        <h1>{{ $heroSectionData['title'] }}</h1>
-                        <div class="row h-100 align-items-center justify-content-center text-center">
-                            <div class="col-12 col-md-9 col-lg-7">
-                                <p class="mt-30 slide-hint">{!! nl2br($heroSectionData['description']) !!}</p>
-
-                                <form action="/search" method="get" class="d-inline-flex mt-30 mt-lg-50 w-100">
-                                    <div class="form-group d-flex align-items-center m-0 slider-search p-10 bg-white w-100">
-                                        <input type="text" name="search" class="form-control border-0 mr-lg-50" placeholder="{{ trans('home.slider_search_placeholder') }}"/>
-                                        <button type="submit" class="btn btn-primary rounded-pill">{{ trans('home.find') }}</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                @endif
-            </div>
-        </section>
+                </div>
+            @endif
+        </div>
+    </section>
     @endif
 
     {{-- Custom | Empowering Students --}}
